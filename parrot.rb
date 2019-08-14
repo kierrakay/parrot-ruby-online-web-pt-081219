371526e6b172909f77d118b6744e48a95152eb2b
Kierra M <k.merriman32@hotmail.com>
2019-08-14 21:41:41 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,12 +1,13 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-#def parrot (phrase = "Squawk!")
-# puts (phrase)
- # return phrase
-#  "Pretty bird!"
-#end
+
   def parrot(phrase = "Squawk!")
     puts phrase
     return phrase
   end
-  parrot("Pretty bird!")
+ 
+ #def parrot(phrase = "Squawk!")
+ #   puts phrase
+  #  return phrase
+  #end
+  #parrot("Pretty bird!") <-- dont need this line because its already written in test spec
