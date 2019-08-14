f41785479296b62d0e856812f3aac502a5d73f80
Kierra M <k.merriman32@hotmail.com>
2019-08-14 21:43:28 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -8,6 +8,6 @@
  
  #def parrot(phrase = "Squawk!")
  #   puts phrase
-  #  return phrase
+  #  return phrase <-- dont need to write return because it's the last line so because puts is nil and there is phrase on bottom it will automatically return last line which is the argument phrase.
   #end
   #parrot("Pretty bird!") <-- dont need this line because its already written in test spec
